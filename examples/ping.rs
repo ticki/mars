109da13d5d5eb0fe79c887157f780e934a52a56c
ticki <ticki@users.noreply.github.com>
2016-05-14 18:11:28 +0200

    Update example

--- examples/ping.rs
+++ examples/ping.rs
@@ -4,8 +4,8 @@ use mars::{Bot, Response};
 
 fn main() {
     Bot::new("TOKENHERE", |_| Response {
-        username: Some("pong-bot"),
+        username: Some("pong-bot".into()),
         text: "pong".into(),
-        icon_url: Some("https://i.ytimg.com/vi/dQw4w9WgXcQ/mqdefault.jpg"),
+        icon_url: Some("https://i.ytimg.com/vi/dQw4w9WgXcQ/mqdefault.jpg".into()),
     }).init("127.0.0.1:80").unwrap();
 }
